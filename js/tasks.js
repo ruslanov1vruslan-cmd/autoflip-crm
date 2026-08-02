bfffee2c75e6236232ae4ab23a143532b1920c1f
ruslanov1vruslan-cmd <ruslanov1vruslan@gmail.com>
2026-08-03 00:30:08 +0300

    Create tasks.js

---
+++ js/tasks.js
@@ -0,0 +1,36 @@
+// AutoFlip CRM 2.0
+// Задачи
+
+
+function showTasks(){
+
+
+document.getElementById("app").innerHTML = `
+
+
+<div class="card">
+
+
+<h2>
+🔧 Задачи
+</h2>
+
+
+<p>
+Активных задач: 0
+</p>
+
+
+</div>
+
+
+<div class="button">
+
+➕ Новая задача
+
+</div>
+
+
+`;
+
+}
