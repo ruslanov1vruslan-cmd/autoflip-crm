50a5333bd8ad341798241aed76e09ab1676a3090
ruslanov1vruslan-cmd <ruslanov1vruslan@gmail.com>
2026-08-03 10:08:46 +0300

    Update cars.js

--- js/cars.js
+++ js/cars.js
@@ -4,20 +4,29 @@
 
 function showCars(){
 
+
 const cars = getCars();
 
 
 let html = `
 
+
 <div class="card">
 
-<h2>🚗 Автомобили</h2>
+<h2>
+🚗 Автомобили
+</h2>
+
 
 <div class="label">
+
 Всего автомобилей: ${cars.length}
-</div>
 
 </div>
+
+
+</div>
+
 
 
 <div class="button" onclick="openNewDeal()">
@@ -26,15 +35,19 @@ let html = `
 
 </div>
 
+
 `;
 
 
 
 if(cars.length === 0){
 
+
 html += `
 
+
 <div class="card">
+
 
 <center>
 
@@ -53,14 +66,18 @@ html += `
 
 </center>
 
+
 </div>
+
 
 `;
 
 }
 
 
+
 cars.forEach(car => {
+
 
 
 html += `
@@ -69,28 +86,56 @@ html += `
 <div class="car-card" onclick="openCarCard(${car.id})">
 
 
+
 <h3>
+
 ${car.brand} ${car.model}
+
 </h3>
 
 
+
 <span class="status ${getStatusClass(car.status)}">
-💰 ${car.status}
+
+${getStatusIcon(car.status)}
+${car.status}
 
 </span>
+
 
 
 <br><br>
 
 
+
 <div>
 
-${car.year} год
+${car.year || "-"} год
 
 </div>
 
 
+
 <br>
+
+
+
+<div>
+
+Пробег:
+
+<b>
+
+${car.mileage || 0} км
+
+</b>
+
+</div>
+
+
+
+<br>
+
 
 
 <div>
@@ -98,63 +143,164 @@ ${car.year} год
 Покупка:
 
 <b>
-${car.buyPrice} ₽
+
+${Number(car.buyPrice || 0).toLocaleString()} ₽
+
 </b>
 
 </div>
 
 
+
 <br>
+
+
+
+<div>
+
+Расходы:
+
+<b>
+
+${Number(car.expenses || 0).toLocaleString()} ₽
+
+</b>
+
+</div>
+
+
+
+<br>
+
 
 
 <div class="profit">
 
+
 ${
-car.profit > 0
+Number(car.profit || 0) > 0
+
 ?
+
 "+" + Number(car.profit).toLocaleString() + " ₽"
+
 :
+
 "В процессе"
+
 }
 
-</div>
-
-
 
 </div>
+
+
+
+</div>
+
 
 
 `;
 
+
+
+});
+
+
+
+document.getElementById("app").innerHTML = html;
+
+
+}
+
+
+
+
+
+// Цвет статуса
+
 function getStatusClass(status){
 
 
-if(status==="В продаже"){
+switch(status){
+
+
+case "В продаже":
 
 return "sale";
 
-}
 
-
-if(status==="Подготовка"){
+case "Подготовка":
 
 return "prepare";
 
-}
 
-
-if(status==="Продано"){
+case "Продано":
 
 return "sale";
 
-}
 
+case "Резерв":
+
+return "prepare";
+
+
+default:
 
 return "buy";
 
 
 }
-});
+
+
+
+}
+
+
+
+
+
+// Иконки статусов
+
+function getStatusIcon(status){
+
+
+switch(status){
+
+
+case "Куплено":
+
+return "💰";
+
+
+case "Подготовка":
+
+return "🔧";
+
+
+case "В продаже":
+
+return "🟢";
+
+
+case "Резерв":
+
+return "🟡";
+
+
+case "Продано":
+
+return "✅";
+
+
+default:
+
+return "🚗";
+
+
+}
+
+
+}
 
 
 
